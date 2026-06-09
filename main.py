436a46b710d514d3af9594537c48a94cd6a220cb
sia <siaalz.gmail.com>
2026-06-09 20:01:55 +0330

    initialized db and articles endpoint

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-def main():
-    print("Hello from personal-blog!")
+import datetime
+import json
+import os
+
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+# Creating DB
+def initiate_db():
+    seed: list = [
+        {
+            "id": 0,
+            "author": "sia",
+            "title": "Welcome",
+            "content": "Hi this is my first app.",
+            "created_at": "2026-06-09T19:22:03.614869",
+        },
+    ]
+
+    os.makedirs("db", exist_ok=True)
+    db_path = os.path.join(os.getcwd(), "db", "db.json")
+    if not os.path.exists(os.path.join(db_path, "db.json")):
+        with open(db_path, mode="w", encoding="utf-8") as file:
+            json.dump(seed, file, indent="")
+
+    return db_path
+
+
+db = initiate_db()
+
+
+@app.get("/")
+async def home():
+    return {"message": "Blog App"}
+
+
+@app.get("/articles")
+async def get_articles():
+
+    with open(db, mode="r", encoding="utf-8") as file:
+        articles = json.load(file)
+        return articles
+
+    # Returns All Articles
